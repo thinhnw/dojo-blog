24a149842ec597824cbec4c2329ef61fb553c921
thinhnw <thinhnv0412@gmail.com>
2024-09-28 18:53:12 +0700

    Add BlogList

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,22 @@
+import { useState } from 'react';
+import BlogList from './BlogList';
+
 const Home = () => {
+
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id)
+    setBlogs(newBlogs)
+  }
+
   return (  
     <div className="home">
-      <h2>Homepage</h2>
+      <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete} />
     </div>
   );
 }
