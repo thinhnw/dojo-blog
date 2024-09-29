bfaee37f47901cb3874c0e0ca05c65739fc197e1
thinhnw <thinhnv0412@gmail.com>
2024-09-29 12:48:14 +0700

    Handle Create form

--- src/Create.js
+++ src/Create.js
@@ -1,14 +1,34 @@
 import { useState } from 'react';
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
+  const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    setIsPending(true);
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',   
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog) 
+    }).then(() => {
+      console.log('new blog added');
+      setIsPending(false);
+      history.push('/');
+    })
+  }
 
   return (  
     <div className="create">
       <h2>Add a new blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input
           type="text"
@@ -30,7 +50,8 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add Blog</button>
+        { !isPending && <button>Add Blog</button> }
+        { isPending && <button disabled>Adding Blog...</button> }
       </form>
     </div>
   );
