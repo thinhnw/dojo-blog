291ece3a74d3502739834dbfe112882a4029202b
thinhnw <thinhnv0412@gmail.com>
2024-09-29 11:09:20 +0700

    Add error handling

--- src/Home.js
+++ src/Home.js
@@ -3,8 +3,9 @@ import BlogList from './BlogList';
 
 const Home = () => {
 
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState(null)
   const [isPending, setIsPending] = useState(true)
+  const [error, setError] = useState(null)
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id)
@@ -13,15 +14,26 @@ const Home = () => {
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
-      .then(res => res.json())
+      .then(res => {
+        if (!res.ok) {
+          throw Error("Could not fetch the data for that resource")
+        }
+        return res.json()
+      })
       .then(data => {
         setBlogs(data)
+        setIsPending(false)
+        setError(null)
+      })
+      .catch(err => {
+        setError(err.message)
         setIsPending(false)
       })
   }, [])
 
   return (  
     <div className="home">
+      { error && <div>{ error }</div> }
       { isPending && <div>Loading...</div> }
       { blogs && <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete} /> }
     </div>
